6ac23ec204054f16db22c851091bc8bf58edbd4a
ezeman <mrnok@yahoo.com>
2025-10-16 11:11:52 +0700

    i18n: upgrade next-intl to v4 and use v4 plugin; restore request-based messages by locale

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,8 +1,6 @@
 import {getRequestConfig} from 'next-intl/server';
 
 export default getRequestConfig(async ({locale}) => {
-  // Temporarily force English messages to isolate runtime error
-  const chosen = 'en';
-  const messages = (await import(`../messages/${chosen}.json`)).default;
+  const messages = (await import(`../messages/${locale}.json`)).default;
   return {messages};
 });
